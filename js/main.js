ddd1a7205b07a4f6ffd1d053f7f4b641b6015896
KaHui Kang <gminions524@naver.com>
2022-08-24 17:50:21 +0900

    modify 1ver

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
 $(document).ready(function(){
 
-     //말풍선클릭
-     $('.base').on('mouseover', function(){
-        $('.click').css({
-            display: 'block'
-        }).on('mouseout', function(){
-            $('click').css({
-                display: 'none'
-            });
-        });
-     });
+  //modal
+  $('.call').click(function(){
+    $("#modal_up").addClass("on");
+  });
+  $(".btn").click(function(){
+    $("#modal_up").removeClass("on");
+  });
 
-    //$('.base').mouseover(function(){
-    //    $(this).find('.click img').addClass("on").fadeIn();
-    //}).mouseout(function(){
-    //    $(this).find('.click img').removeClass("on");
-    //});
+
+
 
 
 
