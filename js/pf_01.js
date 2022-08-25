cef9d2c77c24682dd086e5a9f6ab62bece92242e
KaHui Kang <gminions524@naver.com>
2022-08-25 12:37:07 +0900

    modify 2ver

--- js/pf_01.js
+++ js/pf_01.js
@@ -1,5 +1,78 @@
-$("document").ready(function () {
+$(document).ready(function () {
+    //스크롤효과
+    // let winHeight = $('#section').height();
 
+    //     $(window).on('resize', function () {
+    //         winHeight = $("#section").height();
+    //     });
+
+        // $(window).on('scroll', scrollPage);
+        // scrollPage();
+
+        // $('body').on('mousewheel', function (e, d) {
+        //     if (d > 0) { // 스크롤을 올렸을 때
+        //         let prv = $(this).prev().offset().top;
+        //         $('html,body').stop().animate({
+        //             scrollTop: prv
+        //         });
+        //     } else if (d < 0) { //스크롤을 내렸을 때
+        //         let nxt = $(this).next().offset().top;
+        //         $('html,body').stop().animate({
+        //             scrollTop: nxt
+        //         });
+        //     }
+        // })
+
+
+
+
+
+    // $("body").swipe({
+    //     swipe: function(event, direction){
+    //         if($("#section").is(":animated")){
+    //             return
+    //         }
+    //         if(direction=="up"){
+    //             count++;
+    //             if(count>$("#section").length-1){
+    //                 count=$("#section").length-1;
+    //             }
+
+    //         }else if(direction=="down"){
+    //             count--;
+    //             if(count<0){
+    //                 count=0;
+    //             }
+
+    //         }else if(direction=="left"){
+    //         }else if(direction=="right"){   
+    //         }
+
+    //         $("#section").stop().animate({
+    //             top: -100*count+"%"
+    //         },1000)
+    //     }
+    // })
+    
+    
+
+
+
+
+
+    //skill
+    $(function () {
+        $('.skill_btn>ul>li').click(function () {
+            $(this).addClass("click").siblings().removeClass("click");
+        });
+    });
+
+    $('.skill_btn ul li').eq(0).addClass("click");
+    $('.skill_btn ul li').click(function(){
+        let i=$(this).index();
+        $('.skill_txt p').hide().eq(i).fadeIn();
+        $('.skill_txt p').removeClass("click").eq(i).addClass("click");
+    });
 
 
 });
